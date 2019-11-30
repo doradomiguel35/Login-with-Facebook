214e8f05bab57cc25b41691dccce4325bf443b47
Christian Miguel Dorado <doradomiguel35@gmail.com>
2019-11-30 22:35:56 +0800

    added comments

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,10 +6,13 @@ window.fbAsyncInit = function() {
       version          : 'v5.0'
     });
 };
+
+//List fields to be retrieved after facebook login
 var permissions = `id,first_name,middle_name,last_name,name,email,birthday,picture`;
 var fields = `fields=${permissions}`;
 
 function fbGetUser(){
+    //Fill in query params based on what field will be retrieved
     FB.api(`/me?${fields}`, (res)=>{
         UIkit.notification(`
             <h3 class="uk-text-center">Successfully Logged in! </h3>
@@ -27,9 +30,10 @@ function fbLogin(e){
         if(res.authResponse){
             fbGetUser();
         }else{
+            //Error log in
             console.log(res);
         }
-
+        //Permissions to retrieve listed fields
     }, {scope: 'public_profile,email,user_birthday,user_gender'});
 }
 
